a324c7c8cccd7c7f0dac5c3b93b8274fa986b900
Domiryuu <dawnofmyrequiem@gmail.com>
2017-09-21 16:50:10 -0400

    Update strip-non-utf-8.py
    fixed typo that although would still work was bad.

--- python/strip-non-utf-8.py
+++ python/strip-non-utf-8.py
@@ -2,5 +2,5 @@ f=open('in.txt', "r")
 o=open('out.txt, "w+")
 for line in f:
     line = line.strip()
-    line = line=line.decode('utf-8','ignore').encode("utf-8")
+    line = line.decode('utf-8','ignore').encode("utf-8")
     o.write(line + '\n')
